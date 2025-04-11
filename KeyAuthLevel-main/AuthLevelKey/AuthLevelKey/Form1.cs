bfa1534f83ba1ca7ae821d8d79e4a91c0218f6d7
roaldw <kaooaafqfqfqsqf@gmail.com>
2025-04-10 21:29:11 -0300

    Add files via upload

---
+++ KeyAuthLevel-main/AuthLevelKey/AuthLevelKey/Form1.cs
@@ -0,0 +1,44 @@
+﻿using LicenseAuth;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AuthLevelKey
+{
+    public partial class Form1 : Form
+    {
+
+        public static api LicenseAuthApp = new api(
+    name: "nomorelife",
+    ownerid: "u59wDIUX1e",
+    secret: "41f1688e4bd5dab7ee41adbb735827ebfd63d83bff8b263ac3026173d9c25b94",
+    version: "1.10"
+);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LicenseAuthApp.init();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LicenseAuthApp.license(textBox1.Text);
+            if (LicenseAuthApp.response.success)
+            {
+                Form2 form2 = new Form2();
+                form2.Show();
+                this.Hide();
+            }
+        }
+    }
+}
---
+++ KeyAuthLevel-main/AuthLevelKey/AuthLevelKey/Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AuthLevelKey
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Form1.LicenseAuthApp.user_data.subscriptions[0].subscription == "default")
+            {
+                MessageBox.Show("This Work.");
+            }
+            else
+            {
+                string level = Form1.LicenseAuthApp.user_data.subscriptions[0].subscription;
+                MessageBox.Show($"You Cant Use this because you level is {level}.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Form1.LicenseAuthApp.user_data.subscriptions[0].subscription == "2")
+            {
+                MessageBox.Show("This Work.");
+            }
+            else
+            {
+                string level = Form1.LicenseAuthApp.user_data.subscriptions[0].subscription;
+                MessageBox.Show($"You Cant Use this because you level is {level}.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Form1.LicenseAuthApp.user_data.subscriptions[0].subscription == "3")
+            {
+                MessageBox.Show("This Work.");
+            }
+            else
+            {
+                string level = Form1.LicenseAuthApp.user_data.subscriptions[0].subscription;
+                MessageBox.Show($"You Cant Use this because you level is {level}.");
+            }
+        }
+    }
+}
